aa518f9bf5583629d5c7d271fb870320b216c46f
NextGenWebStudios <reachavigoyal@gmail.com>
2026-03-30 11:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function generateHook(){
 
 const hooks = [
+
 "You won't believe this trick...",
-"This changed everything...",
-"Stop scrolling right now...",
-"Nobody talks about this...",
-"This secret will shock you..."
+"This secret changed everything...",
+"Stop scrolling if you want more views...",
+"99% of creators don't know this...",
+"This hack will blow your mind..."
+
 ];
 
 const random = hooks[Math.floor(Math.random()*hooks.length)];
